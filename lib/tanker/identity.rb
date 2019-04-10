dac23e4266e54becd0bd75cece7227a2d1037ff4
maximerety <maxime.rety@gmail.com>
2019-04-10 18:31:04 +0200

    Add permanent identities

--- lib/tanker/identity.rb
+++ lib/tanker/identity.rb
@@ -1,6 +1,53 @@
 require 'tanker/crypto'
+require 'base64'
+require 'json'
 
 module Tanker
   module Identity
+    BLOCK_HASH_SIZE = 32
+    USER_SECRET_SIZE = 32
+
+    private
+
+    def self.hash_user_id(trustchain_id, user_id)
+      binary_user_id = user_id.dup.force_encoding(Encoding::ASCII_8BIT)
+      Crypto.generichash(binary_user_id + trustchain_id, BLOCK_HASH_SIZE)
+    end
+
+    def self.user_secret(hashed_user_id)
+      random_bytes = Crypto.random_bytes(USER_SECRET_SIZE - 1)
+      check = Crypto.generichash(random_bytes + hashed_user_id, Crypto::HASH_MIN_SIZE)
+      random_bytes + check[0]
+    end
+
+    public
+
+    def self.deserialize(b64_json)
+      JSON.parse(Base64.strict_decode64(b64_json))
+    end
+
+    def self.serialize(hash)
+      Base64.strict_encode64(JSON.generate(hash))
+    end
+
+    def self.create_identity(b64_trustchain_id, b64_trustchain_private_key, user_id)
+      trustchain_id = Base64.strict_decode64(b64_trustchain_id)
+      trustchain_private_key = Base64.strict_decode64(b64_trustchain_private_key)
+
+      hashed_user_id = hash_user_id(trustchain_id, user_id)
+      signature_keypair = Crypto.generate_signature_keypair
+      message = signature_keypair[:public_key] + hashed_user_id
+      signature = Crypto.sign_detached(message, trustchain_private_key)
+
+      serialize({
+        trustchain_id: Base64.strict_encode64(trustchain_id),
+        target: 'user',
+        value: Base64.strict_encode64(hashed_user_id),
+        delegation_signature: Base64.strict_encode64(signature),
+        ephemeral_public_signature_key: Base64.strict_encode64(signature_keypair[:public_key]),
+        ephemeral_private_signature_key: Base64.strict_encode64(signature_keypair[:private_key]),
+        user_secret: Base64.strict_encode64(user_secret(hashed_user_id))
+      })
+    end
   end
 end
--- spec/tanker/identity_spec.rb
+++ spec/tanker/identity_spec.rb
@@ -1,3 +1,63 @@
 # frozen_string_literal: true
 RSpec.describe Tanker::Identity do
+  before(:all) do
+    # Note: vectors below were generated with the Javascript SDK. We keep it to
+    #       check Javascript and Ruby code are interoperable
+    @trustchain = test_trustchain
+    @user_id = 'b_eich'
+    @hashed_user_id = 'RDa0eq4XNuj5tV7hdapjOxhmheTh4QBDNpy4Svy9Xok='
+    @permanent_identity = 'eyJ0cnVzdGNoYWluX2lkIjoidHBveHlOemgwaFU5RzJpOWFnTXZIeXlkK3BPNnpHQ2pPOUJmaHJDTGpkND0iLCJ0YXJnZXQiOiJ1c2VyIiwidmFsdWUiOiJSRGEwZXE0WE51ajV0VjdoZGFwak94aG1oZVRoNFFCRE5weTRTdnk5WG9rPSIsImRlbGVnYXRpb25fc2lnbmF0dXJlIjoiVTlXUW9sQ3ZSeWpUOG9SMlBRbWQxV1hOQ2kwcW1MMTJoTnJ0R2FiWVJFV2lyeTUya1d4MUFnWXprTHhINmdwbzNNaUE5cisremhubW9ZZEVKMCtKQ3c9PSIsImVwaGVtZXJhbF9wdWJsaWNfc2lnbmF0dXJlX2tleSI6IlhoM2kweERUcHIzSFh0QjJRNTE3UUt2M2F6TnpYTExYTWRKRFRTSDRiZDQ9IiwiZXBoZW1lcmFsX3ByaXZhdGVfc2lnbmF0dXJlX2tleSI6ImpFRFQ0d1FDYzFERndvZFhOUEhGQ2xuZFRQbkZ1Rm1YaEJ0K2lzS1U0WnBlSGVMVEVOT212Y2RlMEhaRG5YdEFxL2RyTTNOY3N0Y3gwa05OSWZodDNnPT0iLCJ1c2VyX3NlY3JldCI6IjdGU2YvbjBlNzZRVDNzMERrdmV0UlZWSmhYWkdFak94ajVFV0FGZXh2akk9In0='
+  end
+
+  describe 'parse' do
+    it 'a valid permanent identity' do
+      identity = Tanker::Identity.deserialize(@permanent_identity)
+
+      expect(identity['trustchain_id']).to eq(@trustchain[:id])
+      expect(identity['target']).to eq('user')
+      expect(identity['value']).to eq(@hashed_user_id)
+      expect(identity['delegation_signature']).to eq('U9WQolCvRyjT8oR2PQmd1WXNCi0qmL12hNrtGabYREWiry52kWx1AgYzkLxH6gpo3MiA9r++zhnmoYdEJ0+JCw==')
+      expect(identity['ephemeral_public_signature_key']).to eq('Xh3i0xDTpr3HXtB2Q517QKv3azNzXLLXMdJDTSH4bd4=')
+      expect(identity['ephemeral_private_signature_key']).to eq('jEDT4wQCc1DFwodXNPHFClndTPnFuFmXhBt+isKU4ZpeHeLTENOmvcde0HZDnXtAq/drM3Ncstcx0kNNIfht3g==')
+      expect(identity['user_secret']).to eq('7FSf/n0e76QT3s0DkvetRVVJhXZGEjOxj5EWAFexvjI=')
+    end
+  end
+
+  describe 'permanent identity' do
+    def assert_user_secret(identity)
+      hashed_user_id = Base64.decode64(@identity['value'])
+      user_secret = Base64.decode64(@identity['user_secret'])
+      expect(hashed_user_id.bytesize).to be 32
+      expect(user_secret.bytesize).to be 32
+
+      control_byte = user_secret.bytes.last
+      control = Tanker::Crypto.generichash(user_secret[0..-2] + hashed_user_id, Tanker::Crypto::HASH_MIN_SIZE)
+      expect(control_byte).to eq(control.bytes.first)
+    end
+
+    def assert_signature(identity, trustchain_public_key)
+      signed_data = Base64.decode64(identity['ephemeral_public_signature_key']) +
+                    Base64.decode64(identity['value'])
+
+      signature = Base64.decode64(identity['delegation_signature'])
+      public_key = Base64.decode64(trustchain_public_key)
+
+      expect {
+        Tanker::Crypto.verify_sign_detached(signed_data, signature, public_key)
+      }.not_to raise_exception # no Tanker::Crypto::InvalidSignature
+    end
+
+    before(:all) do
+      @b64_identity = Tanker::Identity.create_identity(@trustchain[:id], @trustchain[:private_key], @user_id)
+      @identity = Tanker::Identity.deserialize(@b64_identity)
+    end
+
+    it 'returns a permanent identity' do
+      expect(@identity.keys.sort).to eq ['delegation_signature', 'ephemeral_private_signature_key', 'ephemeral_public_signature_key', 'target', 'trustchain_id', 'user_secret', 'value']
+      expect(@identity['trustchain_id']).to eq @trustchain[:id]
+      expect(@identity['target']).to eq 'user'
+      assert_user_secret(@identity)
+      assert_signature(@identity, @trustchain[:public_key])
+    end
+  end
 end
